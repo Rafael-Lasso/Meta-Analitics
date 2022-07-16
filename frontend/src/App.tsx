08860d9856e3039df54704d6212d5876319d2676
Rafael Lasso <rafael.comercial27@gmail.com>
2022-07-16 20:12:07 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 
+import NotificationButton from './components/NotificationButton'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <NotificationButton/>
     </>
   )
 }

821393ac510c7891f001e9e43956e454204474e6
Rafael Lasso <rafael.comercial27@gmail.com>
2022-07-16 16:06:54 -0300

    Project created

---
+++ backend/src/main/java/com/metaAnalitics/mAnalitics/MAnaliticsApplication.java
@@ -0,0 +1,13 @@
+package com.metaAnalitics.mAnalitics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MAnaliticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MAnaliticsApplication.class, args);
+	}
+
+}

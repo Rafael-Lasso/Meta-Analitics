4f4db1f28f0c2a6b387b1321d6cb83eede833902
Rafael Lasso <rafael.comercial27@gmail.com>
2022-07-17 10:58:29 -0300

    date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,11 +17,18 @@ function SalesCard() {
 
   const [Sales, setSales] = useState<Sale[]>([]);
 
+  const dmin = minDate.toISOString().slice(0, 10)
+  const dmax = maxDate.toISOString().slice(0, 10)
+
+  console.log(dmin);
+  console.log(dmax);
+  
+
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then((response) => {
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
       setSales(response.data.content);
     });
-  }, []);
+  }, [minDate, maxDate]);
 
   return (
     <>
